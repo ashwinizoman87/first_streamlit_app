c68ef4419818b72797df111663b7c985b23e59fc
Ashwini Vishwasrao Zoman <105253337+ashwinizoman87@users.noreply.github.com>
2022-05-10 05:59:24 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,1 +1,44 @@
 
+import streamlit
+
+
+streamlit.title('My Mom\'s Healthy Diner')
+streamlit.header('Breakfast Favorites')
+streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔 Hard-Boiled Free-Range Egg')
+streamlit.text('🥑🍞 Avocado Toast')
+
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+
+import pandas
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+
+#Choose the Fruit Name Column as the Index
+my_fruit_list = my_fruit_list.set_index('Fruit')
+
+#filter the table data based on the fruits a customer will choose, so we'll pre-populate the list to set an example for the customer. 
+#streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries'])
+
+#We'll ask our app to put the list of selected fruits into a variable called fruits_selected.
+fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries']) #after removing ['Avocado','Strawberries]
+#fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
+fruits_to_show = my_fruit_list.loc[fruits_selected]
+#Then, we'll ask our app to use the fruits in our fruits_selected list to pull rows from the full data set
+streamlit.dataframe(fruits_to_show)
+
+#Put a picklist
+#streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
+
+#display fruit table on page
+#streamlit.dataframe(my_fruit_list)
+
+
+streamlit.header('Fruityvice Fruit Advice')
+
+
+
+fruit_choice = streamlit.text_input('what fruit would you like information about?', 'kiwi')
+streamlit.write('The user entered', fruit_choice)
+                
