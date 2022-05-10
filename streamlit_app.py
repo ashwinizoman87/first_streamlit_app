b425f40f09617f8bbcb3ae5b606d3d7854c8afce
Ashwini Vishwasrao Zoman <105253337+ashwinizoman87@users.noreply.github.com>
2022-05-10 10:24:50 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,9 @@
 
 import streamlit
-
+#import pandas
+#import requests
+#import snowflake.connector
+from urllib.error import URLError
 
 streamlit.title('My Mom\'s Healthy Diner')
 streamlit.header('Breakfast Favorites')
@@ -9,24 +12,20 @@ streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔 Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍞 Avocado Toast')
 
-
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
-import pandas
-my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 
-#Choose the Fruit Name Column as the Index
-my_fruit_list = my_fruit_list.set_index('Fruit')
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+my_fruit_list = my_fruit_list.set_index('Fruit')          #Choose the Fruit Name Column as the Index
 
 #filter the table data based on the fruits a customer will choose, so we'll pre-populate the list to set an example for the customer. 
 #streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries'])
 
 #We'll ask our app to put the list of selected fruits into a variable called fruits_selected.
-fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Banana','Strawberries']) #after removing ['Avocado','Strawberries]
+fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Banana','Strawberries'])   #after removing ['Avocado','Strawberries]
 #fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
 fruits_to_show = my_fruit_list.loc[fruits_selected]
-#Then, we'll ask our app to use the fruits in our fruits_selected list to pull rows from the full data set
-streamlit.dataframe(fruits_to_show)
+streamlit.dataframe(fruits_to_show)   #to display fruit on table
 
 #Put a picklist
 #streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
@@ -34,11 +33,12 @@ streamlit.dataframe(fruits_to_show)
 #display fruit table on page
 #streamlit.dataframe(my_fruit_list)
 
+#new section to display fruitvice api responce
 streamlit.header('Fruityvice Fruit Advice')
 fruit_choice = streamlit.text_input('what fruit would you like information about?', 'Banana')
 streamlit.write('The user entered', fruit_choice)
 
-import requests
+
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
 
 
@@ -48,7 +48,7 @@ fruityvice_normalised = pandas.json_normalize(fruityvice_response.json())
 streamlit.dataframe(fruityvice_normalised)
 
 streamlit.stop()    #dont run anything Past
-import snowflake.connector
+
 
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
